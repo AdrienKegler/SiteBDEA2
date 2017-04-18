27786cb56b3772d2d7e0cadb1a282b6ce0f77097
Dargie <marie.chiaverini@viacesi.fr>
2017-04-18 16:17:48 +0200

    routing event & news + change shophome

---
+++ src/SiteBDE/EventBundle/Controller/EventController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SiteBDE\EventBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class EventController extends Controller
+{
+    public function indexAction()
+    {
+        return $this
+        		->render('SiteBDEEventBundle:Event:Event.html.twig');
+    }
+}
