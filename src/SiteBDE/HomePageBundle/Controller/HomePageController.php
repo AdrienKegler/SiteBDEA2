9abaffffbcbf372e97b34a53a2fed1349932721f
Antoine Caillet <antoine.caillet@viacesi.fr>
2017-04-19 13:41:38 +0200

    commit ou on a fait pas mal de chose mais j'ai pas tout compris :/

--- src/SiteBDE/HomePageBundle/Controller/HomePageController.php
+++ src/SiteBDE/HomePageBundle/Controller/HomePageController.php
@@ -7,9 +7,25 @@ use Symfony\Component\httpFoundation\Response;
 
 class HomePageController extends Controller
 {
-    public function indexAction()
-    {
 
-        return $this->render('SiteBDEHomePageBundle:HomePage:HomePage.html.twig');
-    }
+	public function LoginAction()
+	{	
+		$_SESSION['user']= NULL;
+		return $this->render('SiteBDEHomePageBundle:HomePage:HomePage.html.twig', array("Status" => $_SESSION["Status"], "user" => $_SESSION["user"]));
+	}
+
+	public function SignOutAction()
+	{
+		
+		if (isset($_GET['SignOut'])) {
+			if ($_GET['SignOut'] == 1){
+				$_SESSION["user"] = NULL;
+				$_SESSION["Status"] = NULL;
+			}
+			
+		}	
+	}
+
+
 }
+
