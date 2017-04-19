4d0a122f9625e41b6bce35c67cf03f616663c77a
Dargie <marie.chiaverini@viacesi.fr>
2017-04-19 14:16:27 +0200

    gallery + routing

---
+++ src/SiteBDE/EventBundle/Controller/GalleryController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SiteBDE\EventBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class GalleryController extends Controller
+{
+    public function indexAction()
+    {
+        return $this
+        		->render('SiteBDEEventBundle:Gallery:Gallery.html.twig');
+    }
+    
+}
