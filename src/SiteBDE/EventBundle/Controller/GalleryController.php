b61d5b31e0613fe800a9b8ed2d5dea73a275c6ba
Dargie <marie.chiaverini@viacesi.fr>
2017-04-19 16:35:15 +0200

    Upload photo galerie

--- src/SiteBDE/EventBundle/Controller/GalleryController.php
+++ src/SiteBDE/EventBundle/Controller/GalleryController.php
@@ -1,15 +1,33 @@
 <?php
-
 namespace SiteBDE\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class GalleryController extends Controller
 {
+    
+	public function addAction(Request $request)
+	{
+		$advert = new Advert();
+		$form = $this->createForm(new AdvertType(), $advert);
+
+
+		if ($form->handleRequest($request)->isValid())
+		{
+	
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($advert);
+			$em->flush();
+
+			$request->getSession()->getFlashBag()->add('notice', 'Image bien enregistrée.');
+		}
+
+		return $this->render('SiteBDEEventBundle:Gallery:Gallery.html.twig', $form);
+	}
+
+
     public function indexAction()
     {
-        return $this
-        		->render('SiteBDEEventBundle:Gallery:Gallery.html.twig');
+        return $this->render('SiteBDEEventBundle:Gallery:Gallery.html.twig');
     }
-    
 }
