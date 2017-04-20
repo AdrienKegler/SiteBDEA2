01e10fc076ec092a532c55aa6300bc05471427c0
Dargie <marie.chiaverini@viacesi.fr>
2017-04-20 14:46:40 +0200

    gallery

--- src/SiteBDE/EventBundle/Controller/GalleryController.php
+++ src/SiteBDE/EventBundle/Controller/GalleryController.php
@@ -1,37 +1,17 @@
 <?php
+
+
 namespace SiteBDE\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class GalleryController extends Controller
 {
-    public function addAction()
-  {
-    // On crée un objet Advert
-    $advert = new Advert();
-
-    // On crée le FormBuilder grâce au service form factory
-    $formBuilder = $this->get('form.factory')->createBuilder('form', $advert);
-
-    // On ajoute les champs de l'entité que l'on veut à notre formulaire
-    $formBuilder
-      ->add('date',      'date')
-      ->add('title',     'text')
-      ->add('content',   'textarea')
-      ->add('author',    'text')
-      ->add('published', 'checkbox')
-      ->add('save',      'submit')
-    ;
-    // Pour l'instant, pas de candidatures, catégories, etc., on les gérera plus tard
-
-    // À partir du formBuilder, on génère le formulaire
-    $form = $formBuilder->getForm();
-
-    // On passe la méthode createView() du formulaire à la vue
-    // afin qu'elle puisse afficher le formulaire toute seule
-    return $this->render('OCPlatformBundle:Advert:add.html.twig', array(
-      'form' => $form->createView(),
-    ));
-  }
+	
+    public function indexAction()
+    {
+        return $this
+        		->render('SiteBDEEventBundle:Gallery:Gallery.html.twig');
+    }
 
 }
--- src/SiteBDE/EventBundle/Entity/Photos.php
+++ src/SiteBDE/EventBundle/Entity/Photos.php
@@ -14,10 +14,6 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 class Photos
 {
 
-    private $file;
-
-    private $tempFilename;
-
     /**
      * @var int
      *
@@ -38,9 +34,7 @@ class Photos
   /**
    * @ORM\Column(name="alt", type="string", length=255)
    */
-    private $alt;
-
-    private $file;
+  
 
     private $photo;
 
@@ -50,7 +44,7 @@ class Photos
      * @ORM\Column(name="EventID", type="integer")
      */
     private $eventID;
-
+    
 
     /**
      * Get id
@@ -120,64 +114,6 @@ class Photos
         return $this->eventID;
     }
 
-    public function setFile(UploadedFile $file)
-    {
-        $this->file = $file;
-        if (null !== $this->url)
-        {
-            $this->tempFilename = $this->url;
-
-            $this->url = null;
-            $this->alt = null;
-        }
-    }
-    /**
-   * @ORM\PrePersist()
-   * @ORM\PreUpdate()
-   */
-    public function preUpload()
-    {
-        if (null === $this->file)
-        {
-            return;
-        }
-
-        $this->url = $this->file->guessExtension();
-        $this->alt = $this->file->getClientOriginalName();
-    }
-    /**
-   * @ORM\PostPersist()
-   * @ORM\PostUpdate()
-   */
-    public function upload()
-  {
-    // Si jamais il n'y a pas de fichier (champ facultatif)
-    if (null === $this->file) {
-      return;
-    }
-
-    // Si on avait un ancien fichier, on le supprime
-    if (null !== $this->tempFilename) {
-      $oldFile = $this->getUploadRootDir().'/'.$this->id.'.'.$this->tempFilename;
-      if (file_exists($oldFile)) {
-        unlink($oldFile);
-      }
-    }
-
-    // On déplace le fichier envoyé dans le répertoire de notre choix
-    $this->file->move(
-      $this->getUploadRootDir(), // Le répertoire de destination
-      $this->id.'.'.$this->url   // Le nom du fichier à créer, ici « id.extension »
-    );
-  }
-
-  /**
-   * @ORM\PreRemove()
-   */
-  public function preRemoveUpload()
-  {
-    // On sauvegarde temporairement le nom du fichier, car il dépend de l'id
-    $this->tempFilename = $this->getUploadRootDir().'/'.$this->id.'.'.$this->url;
-  }
+   
 }
 
