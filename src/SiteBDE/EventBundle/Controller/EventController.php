df7ada1c2dba422c7c51e56e917f60490f7e9b60
Antoine Caillet <antoine.caillet@viacesi.fr>
2017-04-20 12:27:16 +0200

    Commit du midi

--- src/SiteBDE/EventBundle/Controller/EventController.php
+++ src/SiteBDE/EventBundle/Controller/EventController.php
@@ -8,8 +8,17 @@ class EventController extends Controller
 {
     public function indexAction()
     {
-        return $this
-        		->render('SiteBDEEventBundle:Event:Event.html.twig');
+    	if (isset($_SESSION["Status"]) && isset($_SESSION["user"])) {
+			if (!is_null($_SESSION["Status"]) && !is_null($_SESSION["user"])) {
+				return $this->render('SiteBDEEventBundle:Event:Event.html.twig', array("Status" => $_SESSION["Status"], "user" => $_SESSION["user"]));
+			}
+			else {
+				return $this->render('SiteBDEEventBundle:Event:Event.html.twig');
+			}
+		}
+		else {
+			return $this->render('SiteBDEEventBundle:Event:Event.html.twig');
+		}
     }
     
 }
--- src/SiteBDE/EventBundle/Controller/EventListController.php
+++ src/SiteBDE/EventBundle/Controller/EventListController.php
@@ -8,7 +8,16 @@ class EventListController extends Controller
 {
     public function EventListAction()
     {
-        return $this
-        		->render('SiteBDEEventBundle:EventList:EventList.html.twig');
-    }
+    	if (isset($_SESSION["Status"]) && isset($_SESSION["user"])) {
+			if (!is_null($_SESSION["Status"]) && !is_null($_SESSION["user"])) {
+				return $this->render('SiteBDEEventBundle:EventList:EventList.html.twig', array("Status" => $_SESSION["Status"], "user" => $_SESSION["user"]));
+			}
+			else {
+				return $this->render('SiteBDEEventBundle:EventList:EventList.html.twig');
+			}
+		}
+		else {
+			return $this->render('SiteBDEEventBundle:EventList:EventList.html.twig');
+		}
+	}
 }
